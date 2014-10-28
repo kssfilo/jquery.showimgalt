86cae9195239e4381f931cc50f7350b0fb59ff37
user <user@euler.local>
2014-10-28 20:06:38 +0900

    Adjusted

--- dist/jquery.showimgalt.js
+++ dist/jquery.showimgalt.js
@@ -1,5 +1,5 @@
-/*! jQuery Img Alt Appender - v0.1.0 - 2014-10-28
-* https://github.com/kssfilo/jquery.showimgalt
+/*! jQuery Img Alt Appender - v0.1.2 - 2014-10-28
+* http://kanasys.com/gtech/
 * Copyright (c) 2014 kssfilo; Licensed MIT */
 (function($){
 	$.fn.showImgAlt=function(options){
